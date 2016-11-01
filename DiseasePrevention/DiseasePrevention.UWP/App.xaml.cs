769f749dc0afcea954aa4f9d9050a13622969ccc
yutelin <yutelin@hotmail.com>
2016-11-01 13:48:02 +0800

    更新套件

--- DiseasePrevention/DiseasePrevention.UWP/App.xaml.cs
+++ DiseasePrevention/DiseasePrevention.UWP/App.xaml.cs
@@ -7,6 +7,9 @@ using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -39,6 +42,21 @@ namespace DiseasePrevention.UWP
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
+            #region Mobile customization 客製化狀態列
+
+            if (ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+
+                var statusBar = StatusBar.GetForCurrentView();
+                if (statusBar != null)
+                {
+                    statusBar.BackgroundOpacity = 1;
+                    statusBar.BackgroundColor = Colors.Black;
+                    statusBar.ForegroundColor = Colors.White;
+                }
+            }
+
+            #endregion
 
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
