4cb4c486d3d2a9518a9379264f63efcc472e9d5c
yutelin <yutelin@hotmail.com>
2016-11-09 02:50:01 +0800

    修正 UWP 無法 Binding Title

--- DiseasePrevention/DiseasePrevention/Views/MainNavigationPage.xaml.cs
+++ DiseasePrevention/DiseasePrevention/Views/MainNavigationPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System.Reflection;
+using Prism.Mvvm;
+using Xamarin.Forms;
 
 namespace DiseasePrevention.Views
 {
@@ -7,6 +9,28 @@ namespace DiseasePrevention.Views
         public MainNavigationPage()
         {
             InitializeComponent();
+
+            // UWP 無法 Binding Title
+            if (Device.OS == TargetPlatform.Windows)
+            {
+                this.Pushed += MainNavigationPage_Pushed;
+            }
+        }
+
+        private void MainNavigationPage_Pushed(object sender, NavigationEventArgs e)
+        {
+            var vm = (BindableBase)this.CurrentPage.BindingContext;
+
+            vm.PropertyChanged += (vmSender, args) =>
+            {
+                if (args.PropertyName == "Title")
+                {
+                    var prop = vmSender.GetType().GetRuntimeProperty("Title");
+                    var title = (string)prop.GetValue(vmSender);
+                    this.CurrentPage.Title = title; // UriKind.Absolute
+                    this.Title = title; // UriKind.Relative
+                }
+            };
         }
     }
 }
